c713a505e5bbf7a8d4ac59fba04d638938b17214
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2019-11-14 12:27:02 -0600

    Created utilery class

---
+++ src/main/java/com/github/mangelt/common/util/InvoiceUtil.java
@@ -0,0 +1,58 @@
+package com.github.mangelt.common.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Component;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class InvoiceUtil {
+
+	public List<File> decompress(@NonNull File compressedFile, Optional<Consumer<String>> callbackError) {
+		try (ZipFile zipFile = new ZipFile(compressedFile)) {
+
+			return zipFile.stream()
+					.filter(zipEntry->{
+						String fileExtension = FilenameUtils.getExtension(zipEntry.getName());
+						return fileExtension.equalsIgnoreCase("xml");
+					})
+					.flatMap(zipEntry->{
+						try {
+							File file = this.createTmpFile(zipEntry.getName());
+							FileUtils.copyInputStreamToFile(zipFile.getInputStream(zipEntry), file);
+							return Stream.of(file);
+						} catch (Exception e) {
+							log.error("Something strange happened to get the File: {}" , zipEntry.getName());
+							log.error("ERROR CLASS: {} ERROR CAUSE: {}" , e.getClass(), e.getCause());
+							if(callbackError.isPresent())
+								callbackError.get().accept(zipEntry.getName());
+							return Stream.empty();
+						}
+					})
+					.collect(Collectors.toList());
+
+		} catch (IOException e) {
+			log.error("There was an error to read the file: {}" , compressedFile.getName());
+		}
+		return null;
+	}
+	
+	public File createTmpFile(String fileName) throws IOException {
+		String baseName = FilenameUtils.getBaseName(fileName);
+		String fileExtension = "."+FilenameUtils.getExtension(fileName);
+		return File.createTempFile(baseName, fileExtension);
+	}
+	
+}
